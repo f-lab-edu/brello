962a3522e62b1117c04c8e1da34e37df35c84dd4
임빛나 <devbit4@gmail.com>
2024-02-23 15:56:27 +0900

    ✅ Zustand 테스트파일 테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import useBearStore from "./store/useBearStore";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const { bears, increaseBear, removeAllBears } = useBearStore();
+
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline my-10">
+        {bears} bears around here...
+      </h1>
+      <button
+        className="border-solid border-2 border-indigo-600 w-48 text-center mx-2"
+        onClick={increaseBear}
+      >
+        one up
+      </button>
+      <button
+        className="border-solid border-2 border-indigo-600 w-48 text-center mx-2"
+        onClick={removeAllBears}
+      >
+        remove all
+      </button>
+    </div>
+  );
 }
