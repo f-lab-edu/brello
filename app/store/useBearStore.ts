568c3bf06105880a7054bffc40cf3f718e969804
임빛나 <devbit4@gmail.com>
2024-02-26 11:26:21 +0900

    ✅ Zustand 테스트파일 타입 추가

--- app/store/useBearStore.ts
+++ app/store/useBearStore.ts
@@ -1,6 +1,12 @@
 import { create } from "zustand";
 
-const useBearStore = create((set) => ({
+type BearStore = {
+  bears: number;
+  increaseBear: () => void;
+  removeAllBears: () => void;
+};
+
+const useBearStore = create<BearStore>((set) => ({
   bears: 0,
   increaseBear: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
