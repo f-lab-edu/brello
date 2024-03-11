82ccb5c1f87f638cd9780f5f7ff00bcb4596a290
임빛나 <devbit4@gmail.com>
2024-03-11 19:45:11 +0900

    ✨ Board drag and drop 기능 추가

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -1,36 +1,59 @@
-import TodoItem from "./TodoItem";
-import Button from "./Button";
+"use client";
 
-export default function TodoList() {
+import { Droppable } from "react-beautiful-dnd";
+import Button from "./Button";
+import TodoItem from "./TodoItem";
+
+import useBoardStore, { Todo } from "../store/useBoardStore";
+
+type TodoListProps = {
+  boardId: string;
+  toDos: Todo[];
+};
+
+export default function TodoList({ boardId, toDos }: TodoListProps) {
+  const { boards, setBoards } = useBoardStore();
+
+  const hanldeItemAdd = () => {
+    const id = Date.now();
+    const text = prompt("텍스트를 입력하세요.");
+
+    if (!text) return;
+
+    const sourceBoardTodos = [...boards[boardId]];
+
+    setBoards({
+      ...boards,
+      [boardId]: [...sourceBoardTodos, { id, text }],
+    });
+  };
+
   return (
     <div className="border-solid border-2 bg-gray-100 mx-2 min-w-[270px] rounded-lg p-4 max-h-full flex flex-col">
-      <h3 className="font-semibold mb-4">타이틀</h3>
-      <ul className="overflow-y-scroll">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+      <h3 className="font-semibold mb-4">{boardId}</h3>
+      <Droppable droppableId={boardId}>
+        {(provided) => {
+          return (
+            <ul
+              className="overflow-y-scroll min-h-[20px]"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {toDos.map((toDo, index) => {
+                return <TodoItem key={toDo.id} toDo={toDo} index={index} />;
+              })}
 
-        <TodoItem />
-      </ul>
-      <Button label="+ Add a card" hasBorder={false} bgColor="transparent" />
+              {provided.placeholder}
+            </ul>
+          );
+        }}
+      </Droppable>
+      <Button
+        label="+ Add a card"
+        hasBorder={false}
+        bgColor="transparent"
+        onClick={hanldeItemAdd}
+      />
     </div>
   );
 }
--- app/boards/[id]/page.tsx
+++ app/boards/[id]/page.tsx
@@ -1,19 +1,73 @@
 "use client";
 
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+
 import Header from "@/app/_components/Header";
 import Sidebar from "@/app/_components/Sidebar";
 import TodoList from "@/app/_components/TodoList";
 import Button from "@/app/_components/Button";
 
+import useBoardStore from "@/app/store/useBoardStore";
+
 export default function Board() {
+  const { boards, setBoards } = useBoardStore();
+
+  const onDragEnd = ({ source, destination }: DropResult) => {
+    if (!destination) return;
+
+    //내부로 움직일 때
+    if (source.droppableId === destination.droppableId) {
+      const sourceBoardTodos = [...boards[source.droppableId]];
+      const targetTodo = sourceBoardTodos[source.index];
+
+      sourceBoardTodos.splice(source.index, 1);
+      sourceBoardTodos.splice(destination.index, 0, targetTodo);
+
+      setBoards({ ...boards, [source.droppableId]: sourceBoardTodos });
+    }
+
+    //외부로 움직일 때
+    if (source.droppableId !== destination.droppableId) {
+      const sourceBoardTodos = [...boards[source.droppableId]];
+      const destinationBoardTodos = [...boards[destination.droppableId]];
+      const targetTodo = sourceBoardTodos[source.index];
+
+      sourceBoardTodos.splice(source.index, 1);
+      destinationBoardTodos.splice(destination.index, 0, targetTodo);
+
+      setBoards({
+        ...boards,
+        [source.droppableId]: sourceBoardTodos,
+        [destination.droppableId]: destinationBoardTodos,
+      });
+    }
+  };
+
+  const onBoardAdd = () => {
+    const newBoard = `Board${Object.keys(boards).length + 1}`;
+
+    setBoards({ ...boards, [newBoard]: [] });
+  };
+
   return (
     <>
       <Header />
       <div className="flex overflow-hidden h-[calc(100%-46px)]">
         <Sidebar />
         <div className="overflow-x-auto flex items-start h-full p-4">
-          <TodoList />
-          <Button label="+ Add Another List" />
+          <DragDropContext onDragEnd={onDragEnd}>
+            {Object.keys(boards).map((boardId) => {
+              return (
+                <TodoList
+                  key={boardId}
+                  boardId={boardId}
+                  toDos={boards[boardId]}
+                />
+              );
+            })}
+          </DragDropContext>
+
+          <Button label="+ Add Another List" onClick={onBoardAdd} />
         </div>
       </div>
     </>
